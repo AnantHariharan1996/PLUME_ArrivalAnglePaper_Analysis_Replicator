c4ec66b37757fd500214b5ec8eedbb78cf7392b8
Anant Hariharan <anant.hariharan.1996@gmail.com>
2026-01-11 09:46:29 -0800

    Update Compare_Reality_2_GlobalSim_Figure.m

--- Comparisons_Global_Models/Compare_Reality_2_GlobalSim_Figure.m
+++ Comparisons_Global_Models/Compare_Reality_2_GlobalSim_Figure.m
@@ -11,10 +11,17 @@ currfname = CMTFLIST(evnum).name;
 ID = extractBefore(currname,'_CMTSOLUTION');
 
 % load the observations, the gdm52 predictions, the s362ani preds and the gladm25 preds
+obsdir = dir([200703130259Slon_Slat_Stt_Evlon_Evlat_EvDep_]);
 
 OBSFNAME = [''];
 GLADFNAME=[''];
 GDMFNAME = [''];
 S362ANIFNAME = [''];
 
+
+figure(954)
+
+
 end
+
+saveas(gcf,'comparisonglobalpred.png')
